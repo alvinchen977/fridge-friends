00ba9da70dbcbafba59b6158bda794123312403e
Adel Fonseca <adelfons@umich.edu>
2021-10-07 16:33:31 -0400

    Camara

---
+++ FridgeFriend/app/src/main/java/edu/umich/mahira/fridgefriend/PostActivity.kt
@@ -0,0 +1,136 @@
+package edu.umich.mahira.fridgefriend
+
+import android.Manifest
+import android.content.ComponentName
+import android.content.ContentValues
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import edu.umich.mahira.fridgefriend.databinding.ActivityPostBinding
+
+class PostActivity : AppCompatActivity() {
+
+    private lateinit var forCropResult: ActivityResultLauncher<Intent>
+
+    private lateinit var view: ActivityPostBinding
+
+    private var imageUri: Uri? = null
+
+    private fun doCrop(intent: Intent?) {
+        intent ?: run {
+            imageUri?.let { view.previewImage.display(it) }
+            return
+        }
+
+        imageUri?.let {
+            intent.data = it
+            forCropResult.launch(intent)
+        }
+    }
+
+    private fun mediaStoreAlloc(mediaType: String): Uri? {
+        val values = ContentValues()
+        values.put(MediaStore.MediaColumns.MIME_TYPE, mediaType)
+        values.put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_PICTURES)
+
+        return contentResolver.insert(
+            if (mediaType.contains("video"))
+                MediaStore.Video.Media.EXTERNAL_CONTENT_URI
+            else
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            values)
+    }
+
+    private fun initCropIntent(): Intent? {
+        // Is there any published Activity on device to do image cropping?
+        val intent = Intent("com.android.camera.action.CROP")
+        intent.type = "image/*"
+        val listofCroppers = packageManager.queryIntentActivities(intent, 0)
+        // No image cropping Activity published
+        if (listofCroppers.size == 0) {
+            toast("Device does not support image cropping")
+            return null
+        }
+
+        intent.component = ComponentName(
+            listofCroppers[0].activityInfo.packageName,
+            listofCroppers[0].activityInfo.name)
+
+        // create a square crop box:
+        intent.putExtra("outputX", 500)
+            .putExtra("outputY", 500)
+            .putExtra("aspectX", 1)
+            .putExtra("aspectY", 1)
+            // enable zoom and crop
+            .putExtra("scale", true)
+            .putExtra("crop", true)
+            .putExtra("return-data", true)
+
+        return intent
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val cropIntent = initCropIntent()
+        view = ActivityPostBinding.inflate(layoutInflater)
+        setContentView(view.root)
+
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { results ->
+            results.forEach {
+                if (!it.value) {
+                    toast("${it.key} access denied")
+                    finish()
+                }
+            }
+        }.launch(arrayOf(
+            Manifest.permission.CAMERA,
+            Manifest.permission.RECORD_AUDIO,
+            Manifest.permission.READ_EXTERNAL_STORAGE))
+
+        forCropResult =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == RESULT_OK) {
+                    result.data?.data.let {
+                        imageUri?.run {
+                            if (!toString().contains("ORIGINAL")) {
+                                // delete uncropped photo taken for posting
+                                contentResolver.delete(this, null, null)
+                            }
+                        }
+                        imageUri = it
+                        imageUri?.let { view.previewImage.display(it) }
+                    }
+                } else {
+                    Log.d("Crop", result.resultCode.toString())
+                }
+            }
+
+        if (!packageManager.hasSystemFeature(PackageManager.FEATURE_CAMERA_ANY)) {
+            toast("Device has no camera!")
+            return
+        }
+
+        val forTakePicture = registerForActivityResult(ActivityResultContracts.TakePicture()) { success ->
+            if (success) {
+                doCrop(cropIntent)
+            } else {
+                Log.d("TakePicture", "failed")
+            }
+        }
+        Log.d("OpenCam", "success")
+
+        view.addImage.setOnClickListener {
+            println("I work")
+            imageUri = mediaStoreAlloc("image/jpeg")
+            forTakePicture.launch(imageUri)
+        }
+
+    }
+}
