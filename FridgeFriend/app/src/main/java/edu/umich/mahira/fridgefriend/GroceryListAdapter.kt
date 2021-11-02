bd54985b819eba82fcb1d0759f78b3dc0e4f0cdd
Adel Fonseca <adelfons@umich.edu>
2021-11-02 13:28:30 -0400

    Minus buttom

--- FridgeFriend/app/src/main/java/edu/umich/mahira/fridgefriend/GroceryListAdapter.kt
+++ FridgeFriend/app/src/main/java/edu/umich/mahira/fridgefriend/GroceryListAdapter.kt
@@ -1,16 +1,18 @@
 package edu.umich.mahira.fridgefriend
 
 import android.content.Context
+import android.content.Intent
 import android.graphics.Color
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import androidx.core.content.ContextCompat.startActivity
 import edu.umich.mahira.fridgefriend.databinding.ListitemFridgeBinding
 
 class GroceryListAdapter(context: Context, users: ArrayList<Item?>) :
     ArrayAdapter<Item?>(context, 0, users) {
-
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         val listItemView = (convertView?.tag /* reuse binding */ ?: run {
             val rowView = LayoutInflater.from(context).inflate(R.layout.listitem_fridge, parent, false)
@@ -22,6 +24,28 @@ class GroceryListAdapter(context: Context, users: ArrayList<Item?>) :
             listItemView.itemTextView.text = name
             listItemView.numbersItemTextView.text = quantity.toString()
             listItemView.root.setBackgroundColor(Color.parseColor(if (position % 2 == 0) "#E0E0E0" else "#EEEEEE"))
+
+            listItemView.MinusButtom.visibility = View.VISIBLE
+            listItemView.MinusButtom.setOnClickListener { v: View ->
+                if (v.id == R.id.MinusButtom) {
+                    for (i in items) {
+                        if (i != null) {
+                            if (i.name == name) {
+                                i.quantity = i.quantity?.minus(1)
+                                quantity = i.quantity
+                                listItemView.numbersItemTextView.text = quantity.toString()
+                                if(i.quantity == 0){
+                                    items.remove(i)
+                                    val intent = Intent(context, FridgeActivity::class.java)
+                                    context.startActivity(intent)
+                                }
+                                break
+                            }
+                        }
+                    }
+
+                }
+            }
         }
         return listItemView.root
     }
--- FridgeFriend/app/src/main/java/edu/umich/mahira/fridgefriend/Item.kt
+++ FridgeFriend/app/src/main/java/edu/umich/mahira/fridgefriend/Item.kt
@@ -1,5 +1,10 @@
 package edu.umich.mahira.fridgefriend
 
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
+
 class Item(
     var name: String? = null,
     var quantity: Int? = null)
+
+
