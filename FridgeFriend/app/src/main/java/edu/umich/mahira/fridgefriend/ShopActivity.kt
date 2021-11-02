566d80bc08d8c503f2ca2f1c7b83f241e2d206dc
rlpoiri <rlpoiri@umich.edu>
2021-11-02 16:46:29 -0400

    fixed some failed others :'(

--- FridgeFriend/app/src/main/java/edu/umich/mahira/fridgefriend/ShopActivity.kt
+++ FridgeFriend/app/src/main/java/edu/umich/mahira/fridgefriend/ShopActivity.kt
@@ -1,15 +1,14 @@
 // temporary: might use room instead for shopping list
-
+// clear all button******
 package edu.umich.mahira.fridgefriend
 
 import android.app.Activity
 import android.content.Intent
-import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
 import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.observe
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
