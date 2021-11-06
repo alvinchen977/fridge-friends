334b67c18eafe5121bcba70124ea387e3d21201d
mgugin <mgugin@umich.edu>
2021-11-06 19:32:00 -0400

    added bottom bar, made unfinished grocery list ListView

--- FridgeFriend/app/src/main/java/edu/umich/mahira/fridgefriend/MainActivity.kt
+++ FridgeFriend/app/src/main/java/edu/umich/mahira/fridgefriend/MainActivity.kt
@@ -1,9 +1,15 @@
 package edu.umich.mahira.fridgefriend
 
+import GroceryListFragment
+import MyFridgeFragment
+import RecipeFragment
+import SavingsFragment
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     fun startPost(view: View?) = startActivity(Intent(this, PostActivity::class.java))
@@ -11,8 +17,37 @@ class MainActivity : AppCompatActivity() {
     fun viewGraph(view: View?) = startActivity(Intent(this, SpendingGraphActivity::class.java))
     fun startReminder(view: View?) = startActivity(Intent(this, ReminderActivity::class.java))
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recipeFragment=RecipeFragment()
+        val groceryListFragment=GroceryListFragment()
+        val myFridgeFragment=MyFridgeFragment()
+        val savingsFragment=SavingsFragment()
+
+        setCurrentFragment(recipeFragment)
+
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.home->setCurrentFragment(recipeFragment)
+                R.id.grocery_list->setCurrentFragment(groceryListFragment)
+                R.id.my_fridge->setCurrentFragment(myFridgeFragment)
+                R.id.savings->setCurrentFragment(savingsFragment)
+            }
+            true
+        }
+
     }
+
+    private fun setCurrentFragment(fragment:Fragment)=
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment,fragment)
+            commit()
+        }
+
 }
+
+
+
---
+++ FridgeFriend/app/src/main/java/edu/umich/mahira/fridgefriend/MyFridgeFragment.kt
@@ -0,0 +1,5 @@
+import androidx.fragment.app.Fragment
+import edu.umich.mahira.fridgefriend.R
+
+class MyFridgeFragment:Fragment(R.layout.fragment_my_fridge) {
+}
