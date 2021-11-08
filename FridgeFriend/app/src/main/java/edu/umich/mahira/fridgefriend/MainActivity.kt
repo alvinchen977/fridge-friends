da533f4ade82452dd0d8719a72b8e17639c8afe7
mgugin <mgugin@umich.edu>
2021-11-08 14:11:45 -0500

    cleaned up main page, made grocery list better looking

--- FridgeFriend/app/src/main/java/edu/umich/mahira/fridgefriend/MainActivity.kt
+++ FridgeFriend/app/src/main/java/edu/umich/mahira/fridgefriend/MainActivity.kt
@@ -8,6 +8,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ListView
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -17,6 +18,7 @@ class MainActivity : AppCompatActivity() {
     fun viewGraph(view: View?) = startActivity(Intent(this, SpendingGraphActivity::class.java))
     fun startReminder(view: View?) = startActivity(Intent(this, ReminderActivity::class.java))
 
+    private lateinit var listView: ListView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
