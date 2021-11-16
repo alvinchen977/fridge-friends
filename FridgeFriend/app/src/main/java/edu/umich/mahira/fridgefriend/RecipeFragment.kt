57ce80a9382fbc1eaa0bc4684d550c3b540395b1
mahir203 <90275545+mahir203@users.noreply.github.com>
2021-11-15 19:19:47 -0500

    collecting names for the recipes

--- FridgeFriend/app/src/main/java/edu/umich/mahira/fridgefriend/RecipeFragment.kt
+++ FridgeFriend/app/src/main/java/edu/umich/mahira/fridgefriend/RecipeFragment.kt
@@ -1,5 +1,37 @@
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.fragment.app.Fragment
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import edu.umich.mahira.fridgefriend.DisplayScannedItemActivity
 import edu.umich.mahira.fridgefriend.R
+import edu.umich.mahira.fridgefriend.RecipeStore
+import edu.umich.mahira.fridgefriend.RecipeStore.getRecipes
 
 class RecipeFragment:Fragment(R.layout.fragment_recipe) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        RecipeStore.getRecipes(activity?.applicationContext!!){ it ->
+//            val arrayTutorialType = object : TypeToken<Array<Array<String>>>() {}.type
+//            var tutorials: Array<Any> = Gson().fromJson(it.toString(), arrayTutorialType)
+//            val temp = tutorials[0] as Array<String>
+//            val test = temp[1]
+            val list: MutableList<String> = arrayListOf()
+            it.forEachIndexed  { idx, tut ->
+                val tutArray = tut as Array<String>
+                tutArray.forEachIndexed{ index, tut_actual ->
+                    if (index.toInt() == 1){
+                        println("> Item ${index}:\n${tut_actual}")
+                        list.add(tut_actual)
+                    }
+
+                }
+
+            }
+
+        }
+    }
 }
+
---
+++ FridgeFriend/app/src/main/java/edu/umich/mahira/fridgefriend/RecipeStore.kt
@@ -0,0 +1,53 @@
+package edu.umich.mahira.fridgefriend
+
+import android.content.Context
+import android.util.Log
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import org.json.JSONObject
+import org.json.JSONArray
+
+
+
+
+object RecipeStore {
+    private lateinit var queue: RequestQueue
+    private const val serverUrl = "https://18.118.131.242/"
+    fun getRecipes(context: Context, completion: (Array<Any>) -> Unit): String {
+        val getRequest = JsonObjectRequest(
+            Request.Method.GET,
+            RecipeStore.serverUrl + "findRecipeByDefault/", null,
+            { response ->
+                val arrayTutorialType = object : TypeToken<Array<Array<String>>>() {}.type
+                var tutorials: Array<Any> = Gson().fromJson(response["recipes"].toString(), arrayTutorialType)
+                val temp = tutorials[0] as Array<String>
+                val test = temp[1]
+                Log.d("getRecipes", test)
+                tutorials.forEachIndexed  { idx, tut ->
+                    val tutArray = tut as Array<String>
+                    tutArray.forEachIndexed{ index, tut_actual ->
+                        println("> Item ${index}:\n${tut_actual}")
+                    }
+
+                }
+                completion(tutorials)
+            },
+            { error ->
+                Log.e(
+                    "getRecipes",
+                    error.localizedMessage ?: "JsonObjectRequest error"
+                )
+            }
+        )
+
+        if (!this::queue.isInitialized) {
+            RecipeStore.queue = Volley.newRequestQueue(context)
+        }
+        RecipeStore.queue.add(getRequest)
+        return "temp"
+    }
+}
